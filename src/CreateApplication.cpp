56df6099be12086077ad3f0a42bd8bbc824d0495
Ta David Yu <ta.yu.gamedev@gmail.com>
2023-05-13 17:26:15 +0200

    Updated application creation interface

--- src/CreateApplication.cpp
+++ src/CreateApplication.cpp
@@ -1,13 +1,26 @@
 #include "AppEntryPoint.h"
 #include "Core/Application.h"
 #include "DYEditorApplication.h"
+#include "Configuration/ProjectConfig.h"
 
 namespace DYE
 {
 	Application * CreateApplication()
 	{
-		// Change the project name here.
-		char const* projectName = "DYEditor Template Project";
-		return new DYE::DYEditor::DYEditorApplication { projectName, 60 };
+		DYEditor::ProjectConfig &runtimeConfig = DYEditor::GetRuntimeConfig();
+		auto projectName = runtimeConfig.GetOrDefault<std::string>(DYEditor::RuntimeConfigKeys::ProjectName, "Sandbox");
+		auto mainWindowWidth = runtimeConfig.GetOrDefault<int>(DYEditor::RuntimeConfigKeys::MainWindowWidth, 1600);
+		auto mainWindowHeight = runtimeConfig.GetOrDefault<int>(DYEditor::RuntimeConfigKeys::MainWindowHeight, 900);
+
+		return new DYEditor::DYEditorApplication
+			{
+				WindowProperties
+					{
+						projectName,
+						(std::uint32_t) mainWindowWidth,
+						(std::uint32_t) mainWindowHeight
+					},
+				60
+			};
 	}
 }
